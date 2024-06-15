3df93e71470b9e5ad92482cc5944905275286568
yasir027 <116998637+yasir027@users.noreply.github.com>
2024-06-15 11:27:13 +0530

    Update HeadnotesContent.jsx

--- components/HeadnotesContent/HeadnotesContent.jsx
+++ components/HeadnotesContent/HeadnotesContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./HeadnotesContent.module.css";
 
-const HeadnotesContent = ({ judgmentData }) => {
+const HeadnotesContent = ({ judgmentData, searchTerms }) => {
   const formatDate = (dateString) => {
     const months = [
       "January", "February", "March", "April", "May", "June",
@@ -15,25 +15,43 @@ const HeadnotesContent = ({ judgmentData }) => {
     const formattedDate = `${toOrdinal(day)} day of ${months[monthIndex]}, ${year}`;
     return formattedDate;
   };
+
   const toOrdinal = (num) => {
     const suffixes = ["th", "st", "nd", "rd"];
     const v = num % 100;
     return num + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
   };
+
+  const highlightText = (text, searchTerms) => {
+    if (!text || !searchTerms.length) return text;
+
+    const regexPattern = searchTerms.map(term => term.replace(/[()]/g, '\\$&')).join('|');
+    const regex = new RegExp(`(${regexPattern})`, 'gi');
+
+    const parts = text.split(regex);
+
+    return parts.map((part, index) =>
+      regex.test(part) ? <mark key={index}>{part}</mark> : part
+    );
+  };
+
   return (
     <div className={styles.scrollableText}>
-
       <div>
         <h3>HEADNOTES</h3>
         {judgmentData && judgmentData.ShortNotes && judgmentData.ShortNotes.length > 0 ? (
           judgmentData.ShortNotes.map((shortNote) => (
             <div key={shortNote.shortNoteId}>
-              <h4>{shortNote.shortNoteText}</h4>
-              {shortNote.LongNotes.map((longNote) =>
-                longNote.LongNoteParas.map((longNotePara) => (
-                  <p key={longNotePara.longNoteParaId}>{longNotePara.longNoteParaText}</p>
-                ))
-              )}
+              <h4>{highlightText(shortNote.shortNoteText, searchTerms)}</h4>
+              {shortNote.LongNotes.map((longNote) => (
+                <div key={longNote.longNoteId}>
+                  {longNote.LongNoteParas.map((longNotePara) => (
+                    <p key={longNotePara.longNoteParaId}>
+                      {highlightText(longNotePara.longNoteParaText, searchTerms)}
+                    </p>
+                  ))}
+                </div>
+              ))}
             </div>
           ))
         ) : (
